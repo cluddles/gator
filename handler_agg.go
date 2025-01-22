de5c779471915e9b1edc2f15377a306558794acd
Dan Fielding <dgfielding@gmail.com>
2025-01-22 22:34:45 +0000

    aggregate loop

--- handler_agg.go
+++ handler_agg.go
@@ -3,28 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
-	"html"
 	"log"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	if len(cmd.args) != 0 {
-		return fmt.Errorf("expects no args")
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("expects one arg")
 	}
 
-	const url = "https://www.wagslane.dev/index.xml"
-	feed, err := fetchFeed(context.Background(), url)
+	timeBetweenReqs, err := time.ParseDuration(cmd.args[0])
 	if err != nil {
 		return err
 	}
 
-	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
-	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
-	for i, _ := range feed.Channel.Item {
-		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
-		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	if timeBetweenReqs < time.Second*5 {
+		return fmt.Errorf("time between requests cannot be below 5 seconds")
 	}
 
-	log.Printf("%v\n", feed)
+	log.Printf("Collecting feeds every %v", timeBetweenReqs)
+	ticker := time.NewTicker(timeBetweenReqs)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
+	ctx := context.Background()
+	feed, err := s.db.GetNextFeedToFetch(ctx)
+	if err != nil {
+		return err
+	}
+
+	s.db.MarkFeedFetched(ctx, feed.ID)
+
+	rssFeed, err := fetchFeed(ctx, feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for i := range rssFeed.Channel.Item {
+		log.Println(rssFeed.Channel.Item[i].Title)
+	}
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -17,3 +17,12 @@ WHERE url = $1;
 -- name: GetFeeds :many
 SELECT feeds.name, feeds.url, users.name AS user_name FROM feeds
 JOIN users ON users.id = feeds.user_id;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds SET last_fetched_at = NOW(), updated_at = NOW() WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at DESC
+NULLS FIRST
+LIMIT 1;
